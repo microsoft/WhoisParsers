cb2c7c4dc7c15f67b58fad28ac8e5ada11f4a8f3
zmarty <contact@ovidiudan.com>
2016-06-18 19:43:21 -0700

    Tweak csproj, add NUnit support

--- WhoisUtils.Tests/IPUtilsTests.cs
+++ WhoisUtils.Tests/IPUtilsTests.cs
@@ -8,7 +8,19 @@ namespace Microsoft.Geolocation.Whois.Utils.Tests
 {
     using System;
     using System.Net;
+
+    #if !NUNIT
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Category = Microsoft.VisualStudio.TestTools.UnitTesting.DescriptionAttribute;
+    #else
+    using NUnit.Framework;
+    using TestInitialize = NUnit.Framework.SetUpAttribute;
+    using TestContext = System.Object;
+    using TestProperty = NUnit.Framework.PropertyAttribute;
+    using TestClass = NUnit.Framework.TestFixtureAttribute;
+    using TestMethod = NUnit.Framework.TestAttribute;
+    using TestCleanup = NUnit.Framework.TearDownAttribute;
+    #endif
 
     [TestClass]
     public class IPUtilsTests
