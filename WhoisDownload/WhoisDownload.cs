4c338867eacf1242d71c04434ff21d5a49cb27c3
ovidan <ovidan@microsoft.com>
2016-08-18 16:23:50 -0700

    Fix downloading LACNIC bulk whois database

--- WhoisDownload/WhoisDownload.cs
+++ WhoisDownload/WhoisDownload.cs
@@ -7,9 +7,13 @@
 namespace Microsoft.Geolocation.Whois.Download
 {
     using System;
+    using System.Collections.Specialized;
     using System.Globalization;
     using System.IO;
+    using System.Net;
+    using System.Text;
     using System.Threading.Tasks;
+    using HtmlAgilityPack;
     using NLog;
 
     public class WhoisDownload
@@ -38,12 +42,55 @@ namespace Microsoft.Geolocation.Whois.Download
             return await DownloadWhoisFile(url, rootPath, outFilename: "apnic.RPSL.db.gz", whoisFolderName: "APNIC", date: date, username: username, password: password);
         }
 
-        public static async Task<bool> DownloadLacnic(string rootPath, string apiKey, DateTime? date = null)
+        public static async Task<bool> DownloadLacnic(string rootPath, string username, string password, DateTime? date = null)
         {
+            /*
             var downloadUrlFormat = "https://lacnic.net/cgi-bin/lacnic/bulkWhoisLoader?stkey={0}&lg=EN";
             var url = string.Format(CultureInfo.InvariantCulture, downloadUrlFormat, apiKey);
 
             return await DownloadWhoisFile(url, rootPath, outFilename: "lacnic.db", whoisFolderName: "LACNIC", date: date);
+            */
+            var lacnicRootUrl = new Uri("https://lacnic.net/");
+            var loginPageUrl = "https://lacnic.net/cgi-bin/lacnic/stini?lg=EN";
+
+            using (var client = new WebClient())
+            {
+                var loginPageHtml = await client.DownloadStringTaskAsync(loginPageUrl);
+
+                var loginPageDoc = new HtmlDocument();
+                loginPageDoc.LoadHtml(loginPageHtml);
+
+                var formNode = loginPageDoc.DocumentNode.SelectSingleNode("//form");
+
+                if (formNode != null)
+                {
+                    var relativeFormUrl = formNode.Attributes["action"].Value;
+                    var absoluteFormUrl = new Uri(lacnicRootUrl, relativeFormUrl);
+
+                    var loginValues = new NameValueCollection();
+                    loginValues.Add("handle", username);
+                    loginValues.Add("passwd", password);
+
+                    client.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+                    var bulkWhoisPageBytes = await client.UploadValuesTaskAsync(absoluteFormUrl, "POST", loginValues);
+                    var bulkWhoisPageHtml = Encoding.UTF8.GetString(bulkWhoisPageBytes);
+
+                    var bulkWhoisPageDoc = new HtmlDocument();
+                    bulkWhoisPageDoc.LoadHtml(bulkWhoisPageHtml);
+
+                    var bulkLinkNode = bulkWhoisPageDoc.DocumentNode.SelectSingleNode("//li/a[1]");
+
+                    if (bulkLinkNode != null)
+                    {
+                        var relativeBulkLinkUrl = bulkLinkNode.Attributes["href"].Value;
+                        var absoluteBulkLinkUrl = new Uri(lacnicRootUrl, relativeBulkLinkUrl);
+
+                        return await DownloadWhoisFile(absoluteBulkLinkUrl.ToString(), rootPath, outFilename: "lacnic.db", whoisFolderName: "LACNIC", date: date);
+                    }
+                }
+            }
+
+            return false;
         }
 
         public static async Task<bool> DownloadWhoisFile(string url, string rootPath, string outFilename, string whoisFolderName, DateTime? date = null, string username = null, string password = null)
