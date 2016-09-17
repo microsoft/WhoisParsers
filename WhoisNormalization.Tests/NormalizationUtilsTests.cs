e79c51b0332a19f1092ef9a18cc9738c0c236bd5
ovidan <ovidan@microsoft.com>
2016-09-17 13:29:13 -0700

    Added date parsing for Updated and Created fields

---
+++ WhoisNormalization.Tests/NormalizationUtilsTests.cs
@@ -0,0 +1,39 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="TestParsers.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Microsoft.Geolocation.Whois.Normalization.Tests
+{
+    using System;
+    using System.Collections.Generic;
+
+    #if !NUNIT
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Category = Microsoft.VisualStudio.TestTools.UnitTesting.DescriptionAttribute;
+    #else
+    using NUnit.Framework;
+    using TestInitialize = NUnit.Framework.SetUpAttribute;
+    using TestContext = System.Object;
+    using TestProperty = NUnit.Framework.PropertyAttribute;
+    using TestClass = NUnit.Framework.TestFixtureAttribute;
+    using TestMethod = NUnit.Framework.TestAttribute;
+    using TestCleanup = NUnit.Framework.TearDownAttribute;
+    #endif
+
+    [TestClass]
+    public class NormalizationUtilsTests
+    {
+        [TestMethod]
+        public void TestFindOldestDate()
+        {
+            Assert.IsNull(NormalizationUtils.FindOldestDate(null), "The extracted date should be null because the input text is null");
+            Assert.IsNull(NormalizationUtils.FindOldestDate("	kenken@sfc.wide.ad.jp    2000021 	kenken@sfc.wide.ad.jp    2000210	kenken@sfc.wide.ad.jp    2000-0-10     "), "The extracted date should be null because the input text is invalid");
+
+            Assert.AreEqual("2000-02-10", NormalizationUtils.FindOldestDate("	kenken@sfc.wide.ad.jp    20000210  "), "The extracted date should be 2000-02-10");
+            Assert.AreEqual("2005-02-05", NormalizationUtils.FindOldestDate("ripe-dbm@ripe.net 20010724 hostmaster@ripe.net 20011024 hostmaster@ripe.net 20020805 ripe-dbm@ripe.net 20040503 ripe-dbm@ripe.net 20041229 hostmaster@afrinic.net 20050205"), "The extracted date should be 2005-02-05");
+            Assert.AreEqual("1987-07-08", NormalizationUtils.FindOldestDate("1987-07-08"), "The extracted date should be 1987-07-08");
+        }
+    }
+}
--- WhoisNormalization/NormalizationUtils.cs
+++ WhoisNormalization/NormalizationUtils.cs
@@ -7,9 +7,11 @@
 namespace Microsoft.Geolocation.Whois.Normalization
 {
     using System;
+    using System.Linq;
     using System.Collections.Generic;
     using System.Text;
     using Parsers;
+    using System.Globalization;
 
     public static class NormalizationUtils
     {
@@ -101,13 +103,13 @@ namespace Microsoft.Geolocation.Whois.Normalization
         public static void ExtractCommonRecordMetadata(RawWhoisSection section, string id, HashSet<string> nameFieldNames, ICommonRecordMetadata target)
         {
             target.Id = id;
-            target.Name = NormalizationUtils.FindFirstMatchingFieldValueInRecords(section, nameFieldNames);
-            target.Created = NormalizationUtils.FindFirstMatchingFieldValueInRecords(section, createdFields);
-            target.Updated = NormalizationUtils.FindFirstMatchingFieldValueInRecords(section, updatedFields);
-            target.UpdatedBy = NormalizationUtils.FindFirstMatchingFieldValueInRecords(section, updatedByFields);
-            target.Description = NormalizationUtils.FindFirstMatchingFieldValueInRecords(section, descriptionFields);
-            target.Comment = NormalizationUtils.FindFirstMatchingFieldValueInRecords(section, commentFields);
-            target.Source = NormalizationUtils.FindFirstMatchingFieldValueInRecords(section, sourceFields);
+            target.Name = FindFirstMatchingFieldValueInRecords(section, nameFieldNames);
+            target.Created = FindOldestDate(FindFirstMatchingFieldValueInRecords(section, createdFields));
+            target.Updated = FindOldestDate(FindFirstMatchingFieldValueInRecords(section, updatedFields));
+            target.UpdatedBy = FindFirstMatchingFieldValueInRecords(section, updatedByFields);
+            target.Description = FindFirstMatchingFieldValueInRecords(section, descriptionFields);
+            target.Comment = FindFirstMatchingFieldValueInRecords(section, commentFields);
+            target.Source = FindFirstMatchingFieldValueInRecords(section, sourceFields);
         }
 
         public static void AddToBuilderWithComma(StringBuilder builder, string text)
@@ -125,6 +127,48 @@ namespace Microsoft.Geolocation.Whois.Normalization
 
                     builder.Append(trimmedText);
                 }
+            }
+        }
+
+        public static string FindOldestDate(string text)
+        {
+            if (text == null)
+            {
+                return null;
+            }
+
+            var words = new List<string>(text.Split(new char[] { ' ' }));
+            words = words.Select(word => word.Trim()).ToList<string>();
+            words = words.Where(word => word.Length > 0).ToList<string>();
+
+            DateTime? oldestParsedDate = null;
+
+            string[] dateFormats = { "yyyy-dd-MM", "yyyyddMM" };
+
+            foreach (var word in words)
+            {
+                DateTime currentParsedDate;
+
+                if (DateTime.TryParseExact(s: word, formats: dateFormats, provider: new CultureInfo("en-US"), style: DateTimeStyles.None, result: out currentParsedDate))
+                {
+                    if (oldestParsedDate == null)
+                    {
+                        oldestParsedDate = currentParsedDate;
+                    }
+                    else if (oldestParsedDate < currentParsedDate)
+                    {
+                        oldestParsedDate = currentParsedDate;
+                    }
+                }
+            }
+
+            if (oldestParsedDate.HasValue)
+            {
+                return oldestParsedDate.Value.ToString("yyyy-dd-MM");
+            }
+            else
+            {
+                return null;
             }
         }
     }
