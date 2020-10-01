791ffde49b6d4fec069ec5354ab0a9e40e968ebc
Andrey Efimenko <andrey.efimenko@sitecore.com>
2020-10-01 14:03:00 +0300

    Align .net versions

--- WhoisDatabaseParsers.Tests/TestParsers.cs
+++ WhoisDatabaseParsers.Tests/TestParsers.cs
@@ -10,18 +10,8 @@ namespace Microsoft.Geolocation.Whois.Parsers.Tests
     using System.Collections.Generic;
     using Utils;
 
-    #if !NUNIT
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Category = Microsoft.VisualStudio.TestTools.UnitTesting.DescriptionAttribute;
-    #else
-    using NUnit.Framework;
-    using TestInitialize = NUnit.Framework.SetUpAttribute;
-    using TestContext = System.Object;
-    using TestProperty = NUnit.Framework.PropertyAttribute;
-    using TestClass = NUnit.Framework.TestFixtureAttribute;
-    using TestMethod = NUnit.Framework.TestAttribute;
-    using TestCleanup = NUnit.Framework.TearDownAttribute;
-    #endif
 
     [TestClass]
     public class TestParsers
